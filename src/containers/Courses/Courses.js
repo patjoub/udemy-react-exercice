0cbc912aa53c5c2b822ed0280807df31c8e35250
patjoub <contact@patjoub.net>
2020-09-08 10:03:12 +0200

    cleaning

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -10,17 +10,12 @@ class Courses extends Component {
             { id: 1, title: 'Angular - The Complete Guide' },
             { id: 2, title: 'Vue - The Complete Guide' },
             { id: 3, title: 'PWA - The Complete Guide' }
-        ],
-        currentCourse: null
+        ]      
     }
 
-    componentDidMount(){
-        console.log('[Courses ] componentDidMount',this.props.match.url)
-    }
 
 
     courseSelectHandler(course) {
-        console.log('courseSelectHandler', course.id)
         this.props.history.push({
                 pathname: '/courses/' + course.id,
                 search: '?title=' + course.title
@@ -47,7 +42,6 @@ class Courses extends Component {
                 <h1>Amazing Udemy Courses</h1>
                 <section className="Courses">
                     { courses }
-
                 </section>
                 <Route path={ this.props.match.url +'/:id' } component={ Course } />
             </div>
